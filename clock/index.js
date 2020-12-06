5c388c9142928e78a5c7ce464761a529978e09c7
ishanuzzal <islam15-14475@diu.edu.bd>
2020-12-06 13:14:04 +0600

    javascript added

--- clock/index.js
+++ clock/index.js
@@ -1,9 +1,25 @@
+
+const secondHand=document.querySelector(".second-hand")
+const minuteHand=document.querySelector(".min-hand")
+const hourHand=document.querySelector(".hour-hand")
+
+
 function setDate(){
     const now=new Date();
     const second=now.getSeconds();
-    const secondsDegree=(second/60)*360;
+    const secondsDegree=(second/60)*360+90;
+    secondHand.style.transform=`rotate(${secondsDegree}deg)`;
+
+    const minute=now.getMinutes();
+    const minuteDegree=((minute/60)*360)+90;
+    minuteHand.style.transform=`rotate(${minuteDegree}deg)`;
+
     
-    setInterval(() => {
-        
-    }, 1000);
+    const hoursDegree=((minute/12)*360)+90;
+    hourHand.style.transform=`rotate(${hoursDegree}deg)`;
+
+
+
+    
 }
+setInterval(setDate,1000)
