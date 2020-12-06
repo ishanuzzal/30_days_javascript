7f3f2a9b7ff837a4d6f55686c23a4a420733d554
ishanuzzal <islam15-14475@diu.edu.bd>
2020-12-06 09:57:49 +0600

    clock folder added

---
+++ clock/index.js
@@ -0,0 +1,9 @@
+function setDate(){
+    const now=new Date();
+    const second=now.getSeconds();
+    const secondsDegree=(second/60)*360;
+    
+    setInterval(() => {
+        
+    }, 1000);
+}
